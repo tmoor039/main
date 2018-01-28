d247e68bf147193fde69fc07d30820f1b7c0c9db
tj <tmoor039@fiu.edu>
2018-01-28 18:31:27 +0000

    file restructure/renamed/reoganization

--- advanced_c/programming_III/offhw1-moore-tiago/COP4338 HW1/COP4338 HW1/tempConversion.c
+++ advanced_c/programming_III/offhw1-moore-tiago/COP4338 HW1/COP4338 HW1/tempConversion.c
@@ -1,13 +1,5 @@
 /*I understand that this homework assignment cannot be joint work with another student in the class or whoever previously took this class.
- I hereby certify that I worked on this homework assignment by myself.
- I did not share any part of my code with my classmates and I did not copy any part of the code from anyone else or from Internet.
-
-During the assignment, I only consulted the following book or online sources: (Please list your sources here.)
 																				
-By putting my name below, I understand that the course adopts a zero tolerance policy for cheating. I cannot give code to or receive code from other students.
-I cannot pull code from the internet.
-I understand any ethical violation will be reported to the university academic affairs and will result in an F grade for this course.
-
 Name: Tiago Moore 
 
 Date: 1/24/2015
@@ -17,19 +9,57 @@ Date: 1/24/2015
 
 int main()
 {
-	float fahr, celsius;
+ 	float  fahr, celsius;
 	int lower = 0, upper = 300, step = 20;
+	int menu = 0, input = 0 ; 
 
-	fahr = lower;
-	do
+
+	do{	
+
+	printf("Choose the type of temperature conversion you would like to do: \n");
+
+	printf("1. Celcius to Fahrenheit.\n");
+	printf("2. Fahrenheit  to Celcius.\n");
+	printf("3. Exit.\n" ); 
+
+	scanf("%d",&menu);
+
+	switch(menu)
 	{
-		celsius = (5.0/9.0)*(fahr-32.0);
-		printf("%3.0f %6.1f\n",fahr, celsius);
-		fahr = fahr + step;
-    }
-        while (fahr<= upper);
-    
+	case 1:
+	{
 
+		printf("Enter the temperature you would like to convert: ");
+		scanf("%d",&input);
+		
+		fahr = (input +32)/.55;
+
+		printf(" %d in Celcius is %f in Fahrenheit",input,fahr);
+	break;
+	} 
+
+	case 2:
+	{
+	
+	printf("Enter the temperature you would like to convert: ");
+
+	scanf("%d",&fahr);
+	
+		celsius = 0;	
+		celsius = (5.0/9.0)*(fahr-32.0);
+		  
+		printf(" %d in Fahrenheit is %d in Celcius",fahr,celsius);
+
+	break; 
+	 }
+  
+	default:
+		input = 0; 	
+	}
+	}
+	while( menu != 1 || menu != 2 || menu !=3);	
+
+	
 return 0;
 }
 
