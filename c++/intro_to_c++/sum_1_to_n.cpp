aae7356c6c0c1bccde339dcc1efb018cd1efe6a7
Tiago M <tmoor039@fiu.edu>
2018-02-05 20:15:02 +0000

    Update sum_1_to_n.cpp
    Future modification = Sum 1+2+....n = n(n+1)/2

--- c++/intro_to_c++/sum_1_to_n.cpp
+++ c++/intro_to_c++/sum_1_to_n.cpp
@@ -1,5 +1,6 @@
 //Tiago Moore
-
+// Edited 5 Feb 2018
+// Modify to incorporate formula for sum of natural numbers from 1 to n n(n+1)/2
 //loop.cpp
 
 // this program prompts the user for a number n and returns the sum of the values from 1  to n.
