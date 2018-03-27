0223b7a044726ae6e5fa96dc884c3ecd7d26f357
tiago <tmoor039@fiu.edu>
2018-03-27 08:20:59 +0000

    operating system dev code added

---
+++ c/ubuntu_kernel_code/system_call_dev/syscall/testmycall.c
@@ -0,0 +1,15 @@
+/*---Start of C file------*/
+
+#include<stdio.h>
+
+#include "testmycall.h"
+
+int main(void)
+
+{
+
+printf("%d\n", syscall(__NR_tiago_moore, 3656274));
+
+}
+
+/*---End of C file------*/
---
+++ c/ubuntu_kernel_code/system_call_dev/testmycall.h
@@ -0,0 +1,9 @@
+/*---Start of header file------*/
+
+#include<linux/unistd.h>
+
+#define __NR_tiago_moore 342
+
+extern long int syscall (long int __sysno, ...) __THROW;
+
+/*---End of header file--------*/
