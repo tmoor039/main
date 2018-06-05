e440f362c659c009e974eb2b861da5abeb4cab2e
Tiago M <tmoor039@fiu.edu>
2018-06-05 07:11:33 -0400

    Update Player.java

--- java/java_moderate/interfaces_polymorphism/Asg3/Player.java
+++ java/java_moderate/interfaces_polymorphism/Asg3/Player.java
@@ -1,6 +1,8 @@
 
 /**
  *This creates a player interface to be played with Nim
+ * Declares the avaiable methods for a class that will implement the player interface
+ * This interface is used by the Human class which implements move and printwinner accordingly 
  * @author tmoore
  */
 public interface Player
